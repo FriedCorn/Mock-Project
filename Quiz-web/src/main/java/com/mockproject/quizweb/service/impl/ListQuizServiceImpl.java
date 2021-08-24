059196a3bdfe281ae8f9534a6e2d9c40c28ccf9b
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-08-24 13:23:37 +0700

    Merge

--- Quiz-web/src/main/java/com/mockproject/quizweb/service/impl/ListQuizServiceImpl.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/service/impl/ListQuizServiceImpl.java
@@ -2,23 +2,18 @@ package com.mockproject.quizweb.service.impl;
 
 import com.mockproject.quizweb.domain.ListQuiz;
 import com.mockproject.quizweb.repository.ListQuizRepository;
+import com.mockproject.quizweb.service.ListQuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ListQuizServiceImpl implements ListQuizService {
-     private final ListQuizRepository listQuizRepository;
+    private final ListQuizRepository listQuizRepository;
 
-     @Autowired
-     public ListQuizServiceImpl(ListQuizRepository listQuizRepository) {
-          this.listQuizRepository = listQuizRepository;
-     }
-
-
-     @Override
-     public void create(ListQuiz listQuiz) {
-          listQuizRepository.save(listQuiz);
-     }
+    @Autowired
+    public ListQuizServiceImpl(ListQuizRepository listQuizRepository) {
+        this.listQuizRepository = listQuizRepository;
+    }
 
     @Override
     public ListQuiz getListQuizById(int id) {
