4ab2f3179e34c2e628a459e51f4063924b02a15e
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2021-08-24 09:35:32 +0700

    QuizDelete

--- Quiz-web/src/main/java/com/mockproject/quizweb/service/QuizService.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/service/QuizService.java
@@ -3,5 +3,6 @@ package com.mockproject.quizweb.service;
 import com.mockproject.quizweb.domain.Quiz;
 
 public interface QuizService {
-     void create(Quiz quiz);
+    void create(Quiz quiz);
+    void delete(Quiz quiz);
 }
