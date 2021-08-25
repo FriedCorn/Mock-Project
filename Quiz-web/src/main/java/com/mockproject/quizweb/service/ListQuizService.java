b2cb1ff8ed2654ad98be2505b9806252163d8f2e
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2021-08-25 13:49:37 +0700

    wtf

--- Quiz-web/src/main/java/com/mockproject/quizweb/service/ListQuizService.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/service/ListQuizService.java
@@ -3,14 +3,8 @@ package com.mockproject.quizweb.service;
 import com.mockproject.quizweb.domain.ListQuiz;
 
 public interface ListQuizService {
-<<<<<<< HEAD
-    void create(ListQuiz listQuiz);
-
-    ListQuiz getListQuizById(int id);
-=======
     ListQuiz getListQuizById(int id);
     String getRemainTime(ListQuiz listQuiz, String username);
     void create(ListQuiz listQuiz);
 
->>>>>>> e2eaccbe1bbc8ee73b9fa09ea93a747f871d8d69
 }
