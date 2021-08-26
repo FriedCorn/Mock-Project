e7b51cac8d23bcef554100bff70e0303d3878c44
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2021-08-27 04:56:32 +0700

    Edit Signup

--- Quiz-web/src/main/java/com/mockproject/quizweb/controller/LoginController.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -43,7 +44,11 @@ public class LoginController {
     }
 
     @PostMapping(value = {"/signup"})
-    public ModelAndView postSignup(ModelAndView mv, @ModelAttribute("account") Account account) {
+    public ModelAndView postSignup(ModelAndView mv, @ModelAttribute("account") Account account, @RequestParam("rePassword") String rePassword) {
+        if (!account.getPassword().equals(rePassword)) {
+            mv.addObject("error", "*** Re-entered password doesn't match ***");
+            return mv;
+        }
         if (accountService.findAccountByUsername(account.getUsername()) != null) {
             mv.addObject("error", "*** Username is existed ***");
             // mv.setViewName("signup");
