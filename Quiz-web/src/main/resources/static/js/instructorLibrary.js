ca17b51016a4e314f737f3e2de0ad369ed3babb8
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-08-26 20:22:06 +0700

    View quiz update

--- Quiz-web/src/main/resources/static/js/instructorLibrary.js
+++ Quiz-web/src/main/resources/static/js/instructorLibrary.js
@@ -29,14 +29,16 @@ function addQuestion(quiz, number) {
     question_name.classList.add("question__name");
     question_name.innerHTML = "Question " + (number + 1);
     let btn__controller = document.createElement("div");
-    btn__controller.classList.add("btn__controller");
+    btn__controller.classList.add("btn__controller", "quiz_button");
     btn__controller.id = quiz["id"];
     let btn__remove = document.createElement("button");
     btn__remove.classList.add("btn__remove", "quiz_button");
     btn__remove.innerHTML = "Remove";
+    btn__remove.onclick = function(event) {delete_active(event);};
     let btn__update = document.createElement("button");
-    btn__update.classList.add("btn__update", "quiz_button");
+    btn__update.classList.add("btn__update");
     btn__update.innerHTML = "Update";
+    btn__update.onclick = function(event) {update_active(event);};
     btn__controller.appendChild(btn__remove);
     btn__controller.appendChild(btn__update);
     question__head.appendChild(question_name);
@@ -46,7 +48,7 @@ function addQuestion(quiz, number) {
     question__body.classList.add("question__body");
     question__body.innerHTML = quiz["content"];
 
-    let question__list_answer = createListAnswer(quiz["answers"], quiz["id"]);
+    let question__list_answer = createListAnswer(quiz["answers"]);
 
     question.appendChild(question__head);
     question.appendChild(question__body);
@@ -54,11 +56,9 @@ function addQuestion(quiz, number) {
     return question;
 }
 
-function createListAnswer(list_ans, id) {
+function createListAnswer(list_ans) {
     let question__list_answer = document.createElement("div");
     question__list_answer.classList.add("question__list_answer");
-    question__list_answer.classList.add("question__list_answer");
-    question__list_answer.id = "ans_list_" + id;
     question__list_answer.appendChild(createGroupAnswer(list_ans[0], list_ans[1]));
     question__list_answer.appendChild(createGroupAnswer(list_ans[2], list_ans[3]));
     return question__list_answer;
@@ -93,22 +93,24 @@ function createAnswer(ans) {
     return answer;
 }
 
-$(document).ready(function() {
-    $(".btn__remove").click(function() {
-        console.log(this.parentElement.id);
-        $(document).ready(function() {
-            $.ajax({
-                url: '/quiz/' + list_quiz_id + "/delete/" + this.parentElement.id,
-                type: 'DELETE',
-                success: function(data) {
-                    console.log(data);
-                    loadData(data);
-                },
-                error: function(data) {
-                    console.log("Error");
-                    console.log(data);
-                }
-            });
-        })
-    });
-});
+function delete_active (e) {
+    console.log(e.target.parentElement.id);
+    $(document).ready(function() {
+        $.ajax({
+            url: '/quiz/' + list_quiz_id + "/delete/" + e.target.parentElement.id,
+            type: 'DELETE',
+            success: function(data) {
+                console.log(data);
+                loadData(data);
+            },
+            error: function(data) {
+                console.log("Error");
+                console.log(data);
+            }
+        });
+    })
+}
+
+function update_active (e) {
+    console.log(e.target.parentElement.id);
+}
