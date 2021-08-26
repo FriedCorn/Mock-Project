68ec1b91a414d17175ab6c26c07d263adc039632
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-08-26 15:42:58 +0700

    Student-activity

---
+++ Quiz-web/src/main/java/com/mockproject/quizweb/controller/StudentActivityController.java
@@ -0,0 +1,35 @@
+package com.mockproject.quizweb.controller;
+
+import com.mockproject.quizweb.domain.ListQuiz;
+import com.mockproject.quizweb.service.QuizHistoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequestMapping("/student/activity")
+public class StudentActivityController {
+    private final QuizHistoryService quizHistoryService;
+
+    public StudentActivityController(QuizHistoryService quizHistoryService) {
+        this.quizHistoryService = quizHistoryService;
+    }
+
+    @GetMapping("/running")
+    public String getRunning(Model model, Principal principal) {
+        List<ListQuiz> quizList = quizHistoryService.getDoingQuizzes(principal.getName());
+        model.addAttribute("listQuizList", quizList);
+        return "studentRunning";
+    }
+
+    @GetMapping("/completed")
+    public String getCompleted(Model model, Principal principal) {
+        List<ListQuiz> quizList = quizHistoryService.getFinishedQuizzes(principal.getName());
+        model.addAttribute("listQuizList", quizList);
+        return "studentCompleted";
+    }
+}
