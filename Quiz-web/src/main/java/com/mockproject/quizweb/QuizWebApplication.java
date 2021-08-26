0605415298f910c1115228bb3621c2e998b8d43d
HarrisTran <dangkhoat81@gmail.com>
2021-08-26 22:14:52 +0700

    available image for everewhere need

--- Quiz-web/src/main/java/com/mockproject/quizweb/QuizWebApplication.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/QuizWebApplication.java
@@ -2,10 +2,10 @@ package com.mockproject.quizweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class QuizWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QuizWebApplication.class, args);
     }
