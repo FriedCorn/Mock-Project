8abcba4af40f0d67c5e5d6070e1f596676d01cb0
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-08-26 19:31:54 +0700

    View quiz update

--- Quiz-web/src/main/java/com/mockproject/quizweb/controller/ViewQuizController.java
+++ Quiz-web/src/main/java/com/mockproject/quizweb/controller/ViewQuizController.java
@@ -2,25 +2,26 @@ package com.mockproject.quizweb.controller;
 
 import com.mockproject.quizweb.domain.ListQuiz;
 import com.mockproject.quizweb.domain.Quiz;
-import com.mockproject.quizweb.domain.QuizHistory;
 import com.mockproject.quizweb.service.ListQuizService;
+import com.mockproject.quizweb.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.Hashtable;
 import java.util.List;
 
 @Controller
 @RequestMapping("/quiz")
 public class ViewQuizController {
     private final ListQuizService listQuizService;
+    private final QuizService quizService;
 
     @Autowired
-    public ViewQuizController(ListQuizService listQuizService) {
+    public ViewQuizController(ListQuizService listQuizService, QuizService quizService) {
         this.listQuizService = listQuizService;
+        this.quizService = quizService;
     }
 
     @GetMapping("{list_quiz_id}")
@@ -37,6 +38,18 @@ public class ViewQuizController {
     public @ResponseBody List<Quiz>
     getQuiz(@PathVariable Integer list_quiz_id) {
         ListQuiz listQuiz = listQuizService.getListQuizById(list_quiz_id);
+        return listQuiz.getQuizzes();
+    }
+
+    @DeleteMapping(value = "/{list_quiz_id}/delete/{quiz_id}")
+    public  @ResponseBody List<Quiz> deleteQuiz(@PathVariable Integer list_quiz_id,
+                                                @PathVariable Integer quiz_id) {
+        ListQuiz listQuiz = listQuizService.getListQuizById(list_quiz_id);
+        Quiz quiz = quizService.getQuizById(quiz_id);
+        listQuiz.getQuizzes().remove(quiz);
+        listQuiz.setNumberOfQuiz(listQuiz.getNumberOfQuiz() - 1);
+        quiz.setListQuiz(null);
+        quizService.delete(quiz);
         return listQuiz.getQuizzes();
     }
 }
--- Quiz-web/src/main/resources/static/js/instructorLibrary.js
+++ Quiz-web/src/main/resources/static/js/instructorLibrary.js
@@ -2,11 +2,113 @@ $(document).ready(function() {
     $.ajax({
         url: '/quiz/' + list_quiz_id + "/get-all-quiz",
         type: 'GET',
-        success: function() {
+        success: function(data) {
             console.log("Success");
+            loadData(data);
         },
-        error: function() {
+        error: function(data) {
             console.log("Error");
+            console.log(data);
         }
     });
 })
+
+function loadData(data) {
+    let body = $("#quiz-body");
+    for (const [key, quiz] of Object.entries(data)) {
+        body.append(addQuestion(quiz, key));
+    }
+}
+
+function addQuestion(quiz, number) {
+    let question = document.createElement("div");
+    question.classList.add("question");
+    let question__head = document.createElement("div");
+    question__head.classList.add("question__head");
+    let question_name = document.createElement("div");
+    question_name.classList.add("question__name");
+    question_name.innerHTML = "Question " + (number + 1);
+    let btn__controller = document.createElement("div");
+    btn__controller.classList.add("btn__controller");
+    btn__controller.id = quiz["id"];
+    let btn__remove = document.createElement("button");
+    btn__remove.classList.add("btn__remove", "quiz_button");
+    btn__remove.innerHTML = "Remove";
+    let btn__update = document.createElement("button");
+    btn__update.classList.add("btn__update", "quiz_button");
+    btn__update.innerHTML = "Update";
+    btn__controller.appendChild(btn__remove);
+    btn__controller.appendChild(btn__update);
+    question__head.appendChild(question_name);
+    question__head.appendChild(btn__controller);
+
+    let question__body = document.createElement("div");
+    question__body.classList.add("question__body");
+    question__body.innerHTML = quiz["content"];
+
+    let question__list_answer = createListAnswer(quiz["answers"], quiz["id"]);
+
+    question.appendChild(question__head);
+    question.appendChild(question__body);
+    question.appendChild(question__list_answer);
+    return question;
+}
+
+function createListAnswer(list_ans, id) {
+    let question__list_answer = document.createElement("div");
+    question__list_answer.classList.add("question__list_answer");
+    question__list_answer.classList.add("question__list_answer");
+    question__list_answer.id = "ans_list_" + id;
+    question__list_answer.appendChild(createGroupAnswer(list_ans[0], list_ans[1]));
+    question__list_answer.appendChild(createGroupAnswer(list_ans[2], list_ans[3]));
+    return question__list_answer;
+}
+
+function createGroupAnswer(ans1, ans2) {
+    let group_ans = document.createElement("div");
+    group_ans.classList.add("group-ans");
+    group_ans.appendChild(createAnswer(ans1));
+    group_ans.appendChild(createAnswer(ans2));
+    return group_ans;
+}
+
+function createAnswer(ans) {
+    let answer = document.createElement("div");
+    answer.classList.add("answer");
+    let label = document.createElement("label");
+    label.classList.add("col", "col-sm-5", "check_container");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    let checkmark = document.createElement("div");
+    checkmark.classList.add("answer__a", "checkmark");
+    let span = document.createElement("span");
+    span.innerHTML = ans["content"];
+    if (ans["trueAnswer"]) {
+        checkbox.checked = true;
+    }
+    label.appendChild(checkbox);
+    label.appendChild(checkmark);
+    answer.appendChild(label);
+    answer.appendChild(span);
+    return answer;
+}
+
+$(document).ready(function() {
+    $(".btn__remove").click(function() {
+        console.log(this.parentElement.id);
+        $(document).ready(function() {
+            $.ajax({
+                url: '/quiz/' + list_quiz_id + "/delete/" + this.parentElement.id,
+                type: 'DELETE',
+                success: function(data) {
+                    console.log(data);
+                    loadData(data);
+                },
+                error: function(data) {
+                    console.log("Error");
+                    console.log(data);
+                }
+            });
+        })
+    });
+});
